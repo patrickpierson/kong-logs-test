7ba86b2e045aaa5f8f816ea33787039cce49bf5d
Patrick Pierson <patrick.c.pierson@gmail.com>
2017-08-25 21:15:23 -0400

    added some documentation

--- config-status-test.sh
+++ config-status-test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Setup the status endpoint
 curl -i -X POST -k --url http://localhost:8001/apis/ --data 'name=status' --data 'uris=/v1/status' --data 'upstream_url=http://test-web'
 
+# Test it
 curl -v http://localhost:8000/v1/status
--- kong-test-logs.sh
+++ kong-test-logs.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# Start the database
 docker run -d --name kong-database -p 5432:5432 -e "POSTGRES_USER=kong" -e "POSTGRES_DB=kong" postgres:9.4
 
+# Wait for the database
 sleep 5
 
+# Start a ngninx testing backend service
 docker run -d --name test-web -p 80:80 nginx:1.13.3-alpine
 
+# Start the kong migrations
 docker run -it --rm --link kong-database:kong-database -e "KONG_DATABASE=postgres" -e "KONG_PG_HOST=kong-database" kong:0.11-alpine kong migrations up
 
+# Start kong and link to the backend service and database, expose it as well
 docker run -it --name kong --link test-web --link kong-database:kong-database -e "KONG_DATABASE=postgres" -e "KONG_PG_HOST=kong-database" -e "KONG_LOG_LEVEL=error" -p 8000:8000 -p 8443:8443 -p 8001:8001 -p 8444:8444 kong:0.11-alpine
